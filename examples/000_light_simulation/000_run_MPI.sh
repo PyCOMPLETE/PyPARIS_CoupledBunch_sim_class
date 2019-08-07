cf60f4eda7a4f5a1f6af7af1b8f39b974c228327
giadarol <giovanni.iadarola@cern.ch>
2019-08-07 11:09:03 +0200

    Some cleanup

--- examples/000_light_simulation/000_run_MPI.sh
+++ examples/000_light_simulation/000_run_MPI.sh
@@ -5,7 +5,7 @@ export PYTHONPATH=$PYTHONPATH:../../../
 rm simulation_status.sta
 
 # Run Parallel without MPI
-for i in 1 # 2 3
+for i in 1 2 3 4
 do
     echo "Run $i"
     mpiexec -n 6 --oversubscribe python for000_run_sim.py
